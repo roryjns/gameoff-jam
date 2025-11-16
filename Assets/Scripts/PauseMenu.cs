cd81a0b9cd54158c01f8b50d306fe6c79ce18518
Agustín Marsero <amarsero@gmail.com>
2025-11-16 18:59:04 +0100

    Main menu!

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,28 +1,36 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool IsPaused { get; private set; }
-
+    public GameObject Controls;
+    public GameObject ContinueButton;
+    GameObject currentActive = null;
     void Start()
     {
         DontDestroyOnLoad(gameObject);
-        TogglePaused(false);
+        TogglePaused(IsMainMenu());
     }
 
     void Update()
     {
         if (InputSystem.actions["Pause Menu"].WasPressedThisFrame())
-        { 
-            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            if (IsMainMenu())
             {
                 // No hiding pause on Main Menu
-                return; 
+                return;
             }
             TogglePaused();
         }
+    }
+
+    public bool IsMainMenu()
+    {
+        return UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "MainMenu";
     }
 
     public void TogglePaused(bool? forceTo = null)
@@ -34,6 +42,11 @@ public class PauseMenu : MonoBehaviour
         }
         float oneIfNotPaused = IsPaused ? 0.0f : 1.0f;
         Time.timeScale = oneIfNotPaused;
+        if (currentActive != null)
+        {
+            currentActive.SetActive(false);
+        }
+        ContinueButton.SetActive(IsMainMenu());
     }
 
     public void ResumeGame()
@@ -43,5 +56,19 @@ public class PauseMenu : MonoBehaviour
     public void PauseGame()
     {
         TogglePaused(true);
+    }
+    public void ShowControls()
+    {
+        if (currentActive != null)
+        {
+            currentActive.SetActive(false);
+        }
+        Controls.SetActive(true);
+        currentActive = Controls;
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Prototype");
+        TogglePaused(false);
     }
 }
